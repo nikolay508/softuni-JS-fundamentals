7c2247c5d76efc676cfabe141881c28f0a18dc0e
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-04 22:21:27 +0300

    Update Arrays - 07.js

--- Arrays/Arrays - 07.js
+++ Arrays/Arrays - 07.js
@@ -1,19 +1,19 @@
 function task(arr1, arr2){
-    let sum = 0
-    isOk = true
+    let sum = 0;
+    isOk = true;
 
     for(let i = 0; i < arr1.length; i++){
         if(arr1[i] === arr2[i]){
-            sum += Number(arr1[i])
+            sum += Number(arr1[i]);
         }else{
-            isOk = false
-            problem = i
-            break
+            isOk = false;
+            problem = i;
+            break;
         }
     }
     if(isOk){
-        console.log(`Arrays are identical. Sum: ${sum}`)
+        console.log(`Arrays are identical. Sum: ${sum}`);
     }else{
-        console.log(`Arrays are not identical. Found difference at ${problem} index`)
+        console.log(`Arrays are not identical. Found difference at ${problem} index`);
     }
 }
