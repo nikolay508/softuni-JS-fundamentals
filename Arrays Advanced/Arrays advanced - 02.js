30228cb88a81dff3e97e3f386c5ee749480145ad
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-04 22:19:12 +0300

    Update Arrays advanced - 02.js

--- Arrays Advanced/Arrays advanced - 02.js
+++ Arrays Advanced/Arrays advanced - 02.js
@@ -1,11 +1,11 @@
 function task(arr){
-    let arr2 = []
+    let arr2 = [];
     for(let i = 0; i < arr.length; i++){
         if(Number(arr[i]) < 0){
-            arr2.unshift(Number(arr[i]))
+            arr2.unshift(Number(arr[i]));
         }else{
-            arr2.push(Number(arr[i]))
+            arr2.push(Number(arr[i]));
         }
     }
-    console.log(arr2.join('\n'))
+    console.log(arr2.join('\n'));
 }
