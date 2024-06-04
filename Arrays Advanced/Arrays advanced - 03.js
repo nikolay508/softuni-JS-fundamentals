71052e146d2646f380c275a9f9f164acadabb18e
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-04 22:19:32 +0300

    Update Arrays advanced - 03.js

--- Arrays Advanced/Arrays advanced - 03.js
+++ Arrays Advanced/Arrays advanced - 03.js
@@ -1,15 +1,15 @@
 function task(arr){
-    let firstLine = []
-    let secLine = []
-    k = arr[0]
+    let firstLine = [];
+    let secLine = [];
+    k = arr[0];
     let arr2 = arr.slice(1)
     for(let i = 0; i < k; i++){
-        firstLine.push(arr2[i])
+        firstLine.push(arr2[i]);
     }
     for(let j = 0; j < k; j++){
-        secLine.unshift(arr2[arr2.length - 1])
-        arr2.pop()
+        secLine.unshift(arr2[arr2.length - 1]);
+        arr2.pop();
     }
-    console.log(firstLine.join(' '))
-    console.log(secLine.join(' '))
+    console.log(firstLine.join(' '));
+    console.log(secLine.join(' '));
 }
