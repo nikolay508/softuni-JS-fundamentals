c77f0a101dcd2261a0471c85af06ff518a09e22e
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-04 22:21:04 +0300

    Update Arrays - 06.js

--- Arrays/Arrays - 06.js
+++ Arrays/Arrays - 06.js
@@ -1,12 +1,12 @@
 function task(arr){
-    let sumEven = 0
-    let sumOdd = 0
+    let sumEven = 0;
+    let sumOdd = 0;
     for(let i = 0; i < arr.length; i++){
         if(arr[i] % 2 === 0){
-            sumEven += arr[i]
+            sumEven += arr[i];
         }else{
-            sumOdd += arr[i]
+            sumOdd += arr[i];
         }
     }
-    console.log(sumEven - sumOdd)
+    console.log(sumEven - sumOdd);
 }
