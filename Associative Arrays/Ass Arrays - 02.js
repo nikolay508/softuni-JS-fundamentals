3fbc09876576622b8a76c61702da4fed7969149c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-05 22:29:32 +0300

    Update Ass Arrays - 02.js

--- Associative Arrays/Ass Arrays - 02.js
+++ Associative Arrays/Ass Arrays - 02.js
@@ -1,17 +1,17 @@
 function task(arr){
-    let meetings = {}
+    let meetings = {};
 
     for(string of arr){
-        let [day, name] = string.split(' ')
+        let [day, name] = string.split(' ');
         if(!meetings.hasOwnProperty(day)){
-            meetings[day] = name
-            console.log(`Scheduled for ${day}`)
+            meetings[day] = name;
+            console.log(`Scheduled for ${day}`);
         }else{
-            console.log(`Conflict on ${day}!`)
+            console.log(`Conflict on ${day}!`);
         }
     }
 
     for(string in meetings){
-        console.log(`${string} -> ${meetings[string]}`)
+        console.log(`${string} -> ${meetings[string]}`);
     }
 }
