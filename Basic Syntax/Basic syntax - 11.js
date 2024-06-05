07d355741852f89ebd1aef856a2a7eab2700951e
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-05 22:31:59 +0300

    Update Basic syntax - 11.js

--- Basic Syntax/Basic syntax - 11.js
+++ Basic Syntax/Basic syntax - 11.js
@@ -1,12 +1,12 @@
 function task(n) {
-    let sum = 0
-    let count = 1
+    let sum = 0;
+    let count = 1;
     for(num = 1; count <= n; num++) {
         if(num % 2 !== 0){
-            count = count + 1
-            sum = sum + num
-            console.log(num)
+            count = count + 1;
+            sum = sum + num;
+            console.log(num);
         }
     }
-    console.log(`Sum: ` + sum)
+    console.log(`Sum: ` + sum);
 }
