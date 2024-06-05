1e85a4433e994601f9b68d637828d6562408da92
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-05 22:30:57 +0300

    Update Basic syntax - 06.js

--- Basic Syntax/Basic syntax - 06.js
+++ Basic Syntax/Basic syntax - 06.js
@@ -1,33 +1,33 @@
 function task(day, age){
-    let price = 0
+    let price = 0;
     if(day == "Weekday"){
         if(0 <= age  && age <= 18){
-            price = 12
+            price = 12;
         }else if(18 < age && age <= 64){
-            price = 18
+            price = 18;
         }else if(64 < age && age <= 122){
-            price = 12
+            price = 12;
         }
     }else if(day == "Weekend"){
         if(0 <= age && age <= 18){
-            price = 15
+            price = 15;
         }else if(18 < age && age <= 64){
-            price = 20
+            price = 20;
         }else if(64 < age && age <= 122){
-            price = 15
+            price = 15;
         }
     }else if(day == "Holiday"){
         if(0 <= age && age <= 18){
-            price = 5
+            price = 5;
         }else if(18 < age && age <= 64){
-            price = 12
+            price = 12;
         }else if(64 < age && age <= 122){
-            price = 10
+            price = 10;
         }
     }
     if(0 > age || age > 122){
-        console.log(`Error!`)
+        console.log(`Error!`);
     }else{
-        console.log(price + `$`)
+        console.log(price + `$`);
     }
 }
