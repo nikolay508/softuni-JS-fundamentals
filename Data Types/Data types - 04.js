39a8e41c3ee38ae38bb504f362b84e085695a73e
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-07 21:17:27 +0300

    Update Data types - 04.js

--- Data Types/Data types - 04.js
+++ Data Types/Data types - 04.js
@@ -1,8 +1,8 @@
 function task(num1, num2, num3){
-    sum = num1 + num2 + num3
+    sum = num1 + num2 + num3;
     if(Number.isInteger(sum)){
-        console.log(`${sum} - Integer`)
+        console.log(`${sum} - Integer`);
     }else{
-        console.log(`${sum} - Float`)
+        console.log(`${sum} - Float`);
     }
 }
