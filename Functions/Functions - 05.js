b7014411b33674e23893f390afa69dd6db6ffc47
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-07 21:19:52 +0300

    Update Functions - 05.js

--- Functions/Functions - 05.js
+++ Functions/Functions - 05.js
@@ -1,11 +1,11 @@
 function task(numOne, numTwo, operator){
     if(operator === 'multiply'){
-        console.log(numOne * numTwo)
+        console.log(numOne * numTwo);
     }else if(operator === 'divide'){
-        console.log(numOne / numTwo)
+        console.log(numOne / numTwo);
     }else if(operator === 'add'){
-        console.log(numOne + numTwo)
+        console.log(numOne + numTwo);
     }else if(operator === 'subtract'){
-        console.log(numOne - numTwo)
+        console.log(numOne - numTwo);
     }
 }
