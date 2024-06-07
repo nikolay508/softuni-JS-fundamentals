240d4d1af5747ab17040de86ff0143d7eb871312
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-07 21:20:34 +0300

    Update Functions - 06.js

--- Functions/Functions - 06.js
+++ Functions/Functions - 06.js
@@ -1,18 +1,18 @@
 function task(a, b, c){
-    count = 0
+    let count = 0;
     if(a < 0){
-        count += 1
+        count += 1;
     }
     if(b < 0){
-        count += 1
+        count += 1;
     }
     if(c < 0){
-        count += 1
+        count += 1;
     }
 
     if(count === 1 || count === 3){
-        console.log('Negative')
+        console.log('Negative');
     }else{
-        console.log('Positive')
+        console.log('Positive');
     }
 }
