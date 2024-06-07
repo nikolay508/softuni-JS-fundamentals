1ebff4b6d3312ce6a900b4b1662ae9ad84c9138a
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-07 21:19:14 +0300

    Update Functions - 04.js

--- Functions/Functions - 04.js
+++ Functions/Functions - 04.js
@@ -1,13 +1,13 @@
 function task(product, amount){
-    let price = 0
+    let price = 0;
     if(product === 'coffee'){
-        price = 1.50 * amount
+        price = 1.50 * amount;
     }else if(product === 'water'){
-        price = 1.00 * amount
+        price = 1.00 * amount;
     }else if(product === 'coke'){
-        price = 1.40 * amount
+        price = 1.40 * amount;
     }else if(product === 'snacks'){
-        price = 2.00 * amount
+        price = 2.00 * amount;
     }
-    console.log(price.toFixed(2))
+    console.log(price.toFixed(2));
 }
