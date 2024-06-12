fa74cce2a69b9c38058954bd3cf1d903825bca28
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-12 21:46:06 +0300

    Update Mid Exam - 1.SoftUni Reception.js

--- Mid Exam/Mid Exam - 1.SoftUni Reception.js
+++ Mid Exam/Mid Exam - 1.SoftUni Reception.js
@@ -1,15 +1,15 @@
 function task(arr){
-    let arr2 = arr.map(Number)
-    let workPerHour = arr2[0] + arr2[1] + arr2[2]
-    let toDo = arr2[3]
-    let hours = 0
+    let arr2 = arr.map(Number);
+    let workPerHour = arr2[0] + arr2[1] + arr2[2];
+    let toDo = arr2[3];
+    let hours = 0;
 
     for(let done = 0; done < toDo; done += workPerHour){
-        hours++
+        hours++;
         if(hours % 4 === 0){
-            hours++
+            hours++;
         }
     }
 
-    console.log(`Time needed: ${hours}h.`)
+    console.log(`Time needed: ${hours}h.`);
 }
