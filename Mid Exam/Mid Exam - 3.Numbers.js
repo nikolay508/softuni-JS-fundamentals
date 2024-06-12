5b616b4fe59e66e060dfaa48244277f2eb0f36c8
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-12 21:48:55 +0300

    Update Mid Exam - 3.Numbers.js

--- Mid Exam/Mid Exam - 3.Numbers.js
+++ Mid Exam/Mid Exam - 3.Numbers.js
@@ -1,38 +1,38 @@
 function task(arr){
-    let arr2 = arr.split(' ').map(Number)
-    let sum = 0
-    let count = 0
-    let top5 = []
+    let arr2 = arr.split(' ').map(Number);
+    let sum = 0;
+    let count = 0;
+    let top5 = [];
 
     while(true){
         for(let i = 0; i < arr2.length; i++){
-            sum += arr2[i]
+            sum += arr2[i];
         }
-        let average = sum / arr2.length
+        let average = sum / arr2.length;
 
         for(let j = 0; j < arr2.length; j++){
             if(arr2[j] > average){
-                count++
-                top5.push(arr2[j])
+                count++;
+                top5.push(arr2[j]);
             }
         }
 
         if(count === 0){
-            console.log('No')
-            break
+            console.log('No');
+            break;
         }
 
         let result = top5.sort((a, b) => {
-            return b - a
+            return b - a;
         })
 
-        let length = result.length
+        let length = result.length;
         while(length > 5){ 
-            length--
-            result.pop()
+            length--;
+            result.pop();
         }
 
-        console.log(result.join(' '))
-        break
+        console.log(result.join(' '));
+        break;
     }
 }
