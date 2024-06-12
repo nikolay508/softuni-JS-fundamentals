397b97f4224f182f07a4c16351d5581f998dd1a6
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-12 21:46:47 +0300

    Update Mid Exam - 2.Array Modifier.js

--- Mid Exam/Mid Exam - 2.Array Modifier.js
+++ Mid Exam/Mid Exam - 2.Array Modifier.js
@@ -1,29 +1,29 @@
 function task(arr){
-    let arrWork = arr.slice(0)
-    let list = arrWork[0].split(' ').map(Number)
-    arrWork.shift()
+    let arrWork = arr.slice(0);
+    let list = arrWork[0].split(' ').map(Number);
+    arrWork.shift();
 
     for(strings of arrWork){
-        let wholeCommand = strings.split(' ')
-        let command = wholeCommand[0]
+        let wholeCommand = strings.split(' ');
+        let command = wholeCommand[0];
 
-        let index1 = Number(wholeCommand[1])
-        let index2 = Number(wholeCommand[2])
+        let index1 = Number(wholeCommand[1]);
+        let index2 = Number(wholeCommand[2]);
 
-        let number1 = Number(list[index1])
-        let number2 = Number(list[index2])
+        let number1 = Number(list[index1]);
+        let number2 = Number(list[index2]);
 
         if(command === 'end'){
-            break
+            break;
         }else if(command === 'swap'){
-            list.splice(index1, 1, number2)
-            list.splice(index2, 1, number1)
+            list.splice(index1, 1, number2);
+            list.splice(index2, 1, number1);
         }else if(command === 'multiply'){
-            let numToReplace = number1 * number2
-            list.splice(index1, 1, numToReplace)
+            let numToReplace = number1 * number2;
+            list.splice(index1, 1, numToReplace);
         }else if(command === 'decrease'){
-            list = list.map(n => n-1)
+            list = list.map(n => n-1);
         }
     }
-    console.log(list.join(', '))
+    console.log(list.join(', '));
 }
