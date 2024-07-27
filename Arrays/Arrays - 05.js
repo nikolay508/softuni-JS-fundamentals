5b08a78b4cc4952b4b75292ebc11f163de646a04
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-07-27 19:05:42 +0300

    Update Arrays - 05.js

--- Arrays/Arrays - 05.js
+++ Arrays/Arrays - 05.js
@@ -1,9 +1,11 @@
 function task(arr){
     let sum = 0;
+    
     for(let i = 0; i < arr.length; i++){
         if(parseInt(arr[i]) % 2 === 0){
             sum += parseInt(arr[i]);
         }
     }
+    
     console.log(sum);
 }
