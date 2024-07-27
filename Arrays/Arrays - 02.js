897d28d306ac08c3f495db02a16d076d5f4b3e0d
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-07-27 19:05:02 +0300

    Update Arrays - 02.js

--- Arrays/Arrays - 02.js
+++ Arrays/Arrays - 02.js
@@ -1,5 +1,6 @@
 function task(num){
     daysWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    
     if(num > 7 || num < 1){
         console.log('Invalid day!');
     }else{
