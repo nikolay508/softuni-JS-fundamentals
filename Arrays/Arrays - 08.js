f8af89ca83a317d79f52a216f6c91e4500df9ebf
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-07-27 19:06:51 +0300

    Update Arrays - 08.js

--- Arrays/Arrays - 08.js
+++ Arrays/Arrays - 08.js
@@ -1,6 +1,7 @@
 function task(arr) {
     while (arr.length > 1) {
       let temp = [];
+        
       for (let i = 0; i < arr.length - 1; i++) {
         temp.push(arr[i] + arr[i + 1]);
       }
